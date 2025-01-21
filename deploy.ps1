3f43b5d651bdf12d540a562949cd43b5993dab20
Darren Frowen <darren.frowen@capita.com>
2025-01-21 19:34:54 +0000

    Add initial workflow definition

--- deploy.ps1
+++ deploy.ps1
@@ -1,5 +1,9 @@
 # Deploys the Bicep template to Azure.
 
+Param(
+    [Switch]$whatif = $false
+)
+
 # Required. Set the location for the deployment
 $location = "UK South"
 
@@ -8,8 +12,10 @@ $templateFile = 'main.bicep'
 $today = Get-Date -Format 'MM-dd-yyyy'
 $deploymentName = "deploymentscript-$today"
 
+if ($whatif) { Write-Host "##[section] Running What-If" } else { Write-Host "##[section] Running Deployment" }
+
 # Deploy the Bicep template
 New-AzDeployment -Name $deploymentName `
     -Location $location `
     -TemplateFile $templateFile `
-    -Verbose #-WhatIf
+    -Verbose -WhatIf:$whatif
